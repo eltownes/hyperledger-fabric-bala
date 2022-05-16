434208f13ba491208d45c466d2c696e127eead5f
eltownes <50155313+eltownes@users.noreply.github.com>
2022-05-16 16:33:39 -0400

    Update prereq-6-docker-hello.sh

--- Labs-LF/prereq-6-docker-hello.sh
+++ Labs-LF/prereq-6-docker-hello.sh
@@ -5,6 +5,13 @@ echo; echo "***** Version"
 docker --version
 docker-compose --version
 
+# Repository
+sudo apt update
+echo; echo "***** Repo Versions"
+echo "Docker: " $(apt-cache madison docker-ce)
+echo
+echo "Docker-Compose: " $(apt-cache madison docker-compose-plugin)
+
 # hello-world
 echo; echo "Should see the Docker Hello World app w/o sudo"
 docker run hello-world
