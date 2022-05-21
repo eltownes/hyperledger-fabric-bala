370b614779652aa5c51f7eb16713d3589bb09f20
eltownes <50155313+eltownes@users.noreply.github.com>
2022-05-21 07:19:19 -0400

    Init CA deployment

---
+++ CA Deployment/prereq-1-basic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# update database
+set -x ; sudo apt update
+set +x
+
+# typical repo must haves & useful tools
+repoInstalls=("tmux" "tree" "meld")
+
+# loop through the array
+echo
+for i in ${repoInstalls[@]}; do
+   echo; echo "***** Installing $i"
+   set -x ; sudo apt install $i -y
+   set +x
+done
+
+# versions
+echo; echo "***** Versions"
+for i in ${repoInstalls[@]}; do
+   echo; echo "***** $i"
+   case $i in
+      tmux)
+         $i -V
+         ;;
+      *)
+         $i --version
+         ;;
+   esac
+done
